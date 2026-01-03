a25d3ef394f6c88a31095cc5f56d8df79bd42b3f
CHAITHANYA45 <devichaithanya45@gmail.com>
2026-01-03 22:06:18 +0530

    remove unused imports from KafkaConsumer and PatientIntegrationTest

--- analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
@@ -1,6 +1,5 @@
 package com.pm.analyticsservice.kafka;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.protobuf.InvalidProtocolBufferException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
